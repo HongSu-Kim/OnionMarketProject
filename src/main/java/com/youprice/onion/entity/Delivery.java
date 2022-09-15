cfca50d684c3f04595e9b0099d6370a44acd6683
HS <coldghd@gmail.com>
2022-09-15 20:35:10 +0900

    📝 Add query, Add fetchType
    1. Add create, drop table query
    complain, review, inquiry
    2. Add fetchType to entity
    delivery, order, wish

--- src/main/java/com/youprice/onion/entity/Delivery.java
+++ src/main/java/com/youprice/onion/entity/Delivery.java
@@ -10,12 +10,14 @@ import java.io.Serializable;
 public class Delivery implements Serializable {
 
     @Id
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;//주문번호 PK FK
 
     @Embedded
     private Address address;//배송지
+
+    private int delivery_cost;//배송비
 
     private String request;//요청사항
 
