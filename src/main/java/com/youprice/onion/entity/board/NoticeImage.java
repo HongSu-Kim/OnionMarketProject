bf8b361e8a1450a85339f00e3752dab206bb1d03
elbib Jeong <elbib8162@naver.com>
2022-09-27 13:29:48 +0900

    update chat, chatroom, notice, noticeimage entity

--- src/main/java/com/youprice/onion/entity/board/NoticeImage.java
+++ src/main/java/com/youprice/onion/entity/board/NoticeImage.java
@@ -1,5 +1,6 @@
 package com.youprice.onion.entity.board;
 
+import com.youprice.onion.entity.board.Notice;
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -11,12 +12,13 @@ public class NoticeImage {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "notice_image_id")
-    private Integer id; //공지 이미지 번호 PK
+    private Integer id; //공지 이미지번호 PK
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "notice_id")
     private Notice notice; //공지 번호 FK
 
-    private String notice_image_name; //첨부파일명
+    @Column
+    private String noticeImageName; //첨부파일명
 
 }
