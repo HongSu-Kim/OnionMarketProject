c1e6632deb417f995a7a0bd0d65712aa75fb5690
jly789 <jly789@naver.com>
2022-10-12 14:04:34 +0900

    update product

---
+++ src/main/java/com/youprice/onion/repository/member/ProhibitionKeywordRepositoy.java
@@ -0,0 +1,13 @@
+package com.youprice.onion.repository.member;
+import com.youprice.onion.entity.member.ProhibitionKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProhibitionKeywordRepositoy extends JpaRepository<ProhibitionKeyword,Long> {
+
+
+
+
+
+}
