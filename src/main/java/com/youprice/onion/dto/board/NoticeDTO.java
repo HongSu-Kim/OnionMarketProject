d14c3d60088ef7db98e692d5f43658c31aba8b2a
elbib Jeong <elbib8162@naver.com>
2022-10-12 11:57:01 +0900

    change notice controller , noticeDTO, NoticeRepository and so on

--- src/main/java/com/youprice/onion/dto/board/NoticeDTO.java
+++ src/main/java/com/youprice/onion/dto/board/NoticeDTO.java
@@ -1,23 +1,42 @@
 package com.youprice.onion.dto.board;
 
+import com.youprice.onion.entity.board.Notice;
 import com.youprice.onion.entity.board.NoticeType;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Not;
 
 import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
 
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class NoticeDTO {
 
-    @NotEmpty(message = "공지의 타입을 지정해주세요.")
-    private NoticeType noticeType; //공지타입(notice, qna, event ...)
+    private Long noticeId;
 
-    @NotEmpty(message = "제목이 없습니다.")
+    private Long memberId;
+
+    private String noticeType; //공지타입(notice, qna, event ...)
+
     private String noticeSubject; //공지제목
 
-    @NotEmpty(message = "공지에 내용이 없습니다.")
     private String noticeContent; //공지내용
 
+    private LocalDateTime noticeDate; //작성일자
+
+    private int hitCount; //조회수
+
+    public NoticeDTO(Notice notice) {
+        this.noticeId = notice.getId();
+        this.memberId = notice.getMember().getId();
+        this.noticeType = notice.getNoticeType().name();
+        this.noticeSubject = notice.getNoticeSubject();
+        this.noticeContent = notice.getNoticeContent();
+        this.noticeDate = notice.getNoticeDate();
+        this.hitCount = notice.getHitCount();
+    }
 }
--- src/main/java/com/youprice/onion/repository/board/NoticeRepository.java
+++ src/main/java/com/youprice/onion/repository/board/NoticeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, Long> { //JpaRepository<엔티티, pk의 타입>
 
 }
