fa485b890401cc2e7206462365796c24304f4358
HeeSeong Eom <gmltjd9801@naver.com>
2022-10-16 00:08:24 +0900

    fix(add-service): add product

---
+++ src/main/java/com/youprice/onion/dto/product/ProductUpdateDTO.java
@@ -0,0 +1,28 @@
+package com.youprice.onion.dto.product;
+
+import com.youprice.onion.entity.product.ProductProgress;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductUpdateDTO {
+
+    private String productName; //상품명
+    private String subject; //제목
+    private String content; //내용
+    private int price; //상품가격
+    private LocalDateTime updateDate; //수정일
+    private ProductProgress productProgress; //판매상태 Reserved,tradings,soldout 예약중,거래중,판매완료
+    private String payStatus; //페이현황
+    private String blindStatus; //블라인드현황
+    //이미지는 배열로
+    private List<MultipartFile> productImageName;//이미지 이름
+
+}
--- src/main/java/com/youprice/onion/service/product/impl/ProductServiceImpl.java
+++ src/main/java/com/youprice/onion/service/product/impl/ProductServiceImpl.java
@@ -1,81 +1,186 @@
 package com.youprice.onion.service.product.impl;
 
-import com.youprice.onion.dto.product.ProductDTO;
-import com.youprice.onion.dto.product.ProductImageDTO;
+import com.youprice.onion.dto.product.*;
+import com.youprice.onion.entity.member.Member;
 import com.youprice.onion.entity.product.Product;
 import com.youprice.onion.entity.product.ProductImage;
+import com.youprice.onion.entity.product.Town;
+import com.youprice.onion.repository.member.MemberRepository;
+import com.youprice.onion.repository.member.ProhibitionKeywordRepositoy;
 import com.youprice.onion.repository.product.ProductImageRepository;
 import com.youprice.onion.repository.product.ProductRepository;
+import com.youprice.onion.repository.product.TownRepositoy;
 import com.youprice.onion.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-@ToString
+@Slf4j
 public class ProductServiceImpl implements ProductService {
 
+    private final MemberRepository memberRepository;
+    private final TownRepositoy townRepositoy;
     private final ProductRepository productRepository;
-    private final ProductRepository.ProductManager productManager;
     private final ProductImageRepository productImageRepository;
+
+    private final ProhibitionKeywordRepositoy prohibitionKeywordRepositoy;
+
+    private final static String COOKIE = "alreadyViewCookie";
 
     //상품 등록
     @Override
     @Transactional
-    public Long addProduct(ProductDTO productDTO, ProductImageDTO productImageDTO, MultipartFile file)throws  Exception {
+    public Long addProduct(ProductAddDTO productAddDTO, List<MultipartFile> fileList) throws Exception {
 
-        Product product = new Product();
-        ProductImage productImage = new ProductImage();
+        Member member = memberRepository.findById(productAddDTO.getMemberId()).orElse(null);
+        Town town = townRepositoy.findById(91L).orElse(null);
+        //productAddDTO.getTownId()
+        // 상품 등록
+        Product product = new Product(member,town,productAddDTO.getProductName(),productAddDTO.getSubject(),productAddDTO.getContent(),productAddDTO.getPrice(),productAddDTO.getUploadDate());
 
-        product.createProduct(productDTO);
+        Long productId = productRepository.save(product).getId();
 
+        // 상품 이미지 등록
+        //반복으로 꺼내면서 하나씩 저장
+        List<ProductImage> productImageList = productImages(productId,fileList);
+        for(ProductImage productImage : productImageList){
+            productImageRepository.save(productImage);
+        }
+
+        return productId;
+    }
+
+    //상품 수정
+    @Override
+    @Transactional
+    public Long updateProduct(Long productId,ProductUpdateDTO updateDTO) throws Exception {
+
+        Product product = productRepository.findById(productId).orElse(null);
+        product.updateProduct(productId,updateDTO);
+
+        List<ProductImage> productImageList = productImageRepository.findByProductId(productId);
+        for (ProductImage productImage : productImageList){
+            productImageRepository.delete(productImage);
+        }
+        List<ProductImage> imageList = productImages(productId, updateDTO.getProductImageName());
+        for(ProductImage productImage : imageList){
+            productImageRepository.save(productImage);
+        }
         productRepository.save(product);
 
-        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
-
-        String productImageName = file.getOriginalFilename();
-
-        File saveFile = new File(projectPath, productImageName);
-
-        file.transferTo(saveFile);
-        product = productRepository.findById(product.getId()).orElse(null);
-
-        productImage.addProductImage(productImageDTO,product,productImageName);
-
-        productImageRepository.save(productImage);
         return productRepository.save(product).getId();
-    }
-    //전체 상품 조회
-    @Override
-    public List<ProductDTO> getProductList() {
-        return productRepository.findAll().stream()
-                .map(product -> new ProductDTO(product))
-                .collect(Collectors.toList());
-    }
-
-    //상품 하나에 대한 데이터
-    @Override
-    public Optional<Product> findById(Long id) {
-        return productRepository.findById(id);
     }
 
     //상품 삭제
     @Override
     @Transactional
-    public void deleteProduct(Long productId, Long productImageId, MultipartFile file) throws Exception {
+    public void deleteProduct(Long productId) throws Exception {
 
-        File deleteFile = new File(String.valueOf(file));
-        deleteFile.delete();
-        productImageRepository.deleteById(productImageId);
+        productImageRepository.deleteById(productId);
         productRepository.deleteById(productId);
     }
+
+    //전체 상품 조회
+    @Override
+    public List<ProductListDTO> getProductList() {
+        return productRepository.findAll().stream()
+                .map(product -> new ProductListDTO(product))
+                .collect(Collectors.toList());
+    }
+
+    //상품 하나에 대한 데이터
+    @Override
+    public ProductDTO getProductDTO(Long productId) {
+        return productRepository.findById(productId).map(ProductDTO::new).orElse(null);
+    }
+    //이미지리스트
+    public List<ProductImage> productImages(Long productId, List<MultipartFile> fileList) throws Exception{
+        List<ProductImage> productImageList = new ArrayList<>();
+        Product product = productRepository.findById(productId).orElse(null);
+        for(MultipartFile file: fileList) {
+
+            if(!file.isEmpty()) {
+                String productImageName = file.getOriginalFilename();
+                ProductImage saveFile = new ProductImage(product, productImageName);
+                productImageList.add(saveFile);
+            }
+        }
+        return productImageList;
+    }
+
+    //이미지파일 경로,저장
+    public String filePath(MultipartFile multipartFile)throws  Exception{
+        String filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\img\\product";
+
+        if(multipartFile.isEmpty()){
+            return null;
+        }
+
+        String fileName = multipartFile.getOriginalFilename();
+
+        multipartFile.transferTo(new File(filePath, fileName));
+
+        return fileName;
+    }
+    //조회수 증가
+    @Override
+    @Transactional
+    public int updateView(Long productId) {
+        return productRepository.updateView(productId);
+    }
+
+//    @Override
+//    public String getFirstImage(List<ProductImageDTO> productImageList) throws Exception {
+//
+//        for(ProductImageDTO productImage : productImageList){
+//
+//            productImage = prdo;
+//
+//        }
+//
+//        return productImage;
+//    }
+
+    //    @Override
+//    @Transactional
+//    public int updateView(Long productId, HttpServletRequest req, HttpServletResponse resp) {
+//
+//        Cookie[] cookies = req.getCookies();
+//        boolean checkCookie = false;
+//        int result = 0;
+//        if(cookies != null) {
+//            for (Cookie cookie : cookies){
+//                //조회 시 체크 true
+//                if(cookie.getName().equals(COOKIE+productId)) checkCookie = true;
+//            }
+//            if(!checkCookie){
+//                Cookie newCookie = addCookieForOverlap(productId);
+//                resp.addCookie(newCookie);
+//                result = productRepository.updateView(productId);
+//            }
+//        }else {
+//            Cookie newCookie = addCookieForOverlap(productId);
+//            resp.addCookie(newCookie);
+//            result = productRepository.updateView(productId);
+//        }
+//        return result;
+//    }
+//
+//    private Cookie addCookieForOverlap(Long productId) {
+//        Cookie cookie = new Cookie(COOKIE+productId, String.valueOf(productId));
+//        cookie.setComment("조회수 중복 방지");
+//        cookie.setMaxAge(10000);
+//        cookie.setHttpOnly(true);
+//        return cookie;
+//    }
 }
