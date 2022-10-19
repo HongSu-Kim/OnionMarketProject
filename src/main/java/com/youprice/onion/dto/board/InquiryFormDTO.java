cdca7f498b01af2b5c338cc61b0f6f1c5453ec25
Sera Oh <sera16@naver.com>
2022-10-19 20:21:42 +0900

    update complain,review

--- src/main/java/com/youprice/onion/dto/board/InquiryFormDTO.java
+++ src/main/java/com/youprice/onion/dto/board/InquiryFormDTO.java
@@ -1,12 +1,10 @@
 package com.youprice.onion.dto.board;
 
-import com.youprice.onion.entity.member.Member;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -17,7 +15,7 @@ public class InquiryFormDTO {
     @NotEmpty(message = "문의유형을 선택해주세요")
     private String inquiryType; // 문의유형
 
-    @NotEmpty(message = "상세유형을 선택해주세요")
+    @NotEmpty(message = "문의유형을 선택해주세요")
     private String detailType; // 상세유형
 
     @NotEmpty(message = "제목을 입력해주세요")
