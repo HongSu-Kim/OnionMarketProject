e7c9ad75d7ad10d5fa434cdad08b5be86caeb133
HeeSeong Eom <gmltjd9801@naver.com>
2022-10-24 02:22:16 +0900

    fix(update,bid service): update&bid product (not working)

--- src/main/java/com/youprice/onion/repository/product/BiddingRepository.java
+++ src/main/java/com/youprice/onion/repository/product/BiddingRepository.java
@@ -1,4 +1,11 @@
 package com.youprice.onion.repository.product;
 
-public interface BiddingRepository {
+import com.youprice.onion.entity.product.Bidding;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BiddingRepository extends JpaRepository<Bidding, Long> {
+
+
 }
