8733c3f7dd8bfe853067c46aa6d6b93f2b6e83d4
Sera Oh <sera16@naver.com>
2022-10-31 19:33:42 +0900

    update complain

--- src/main/java/com/youprice/onion/dto/board/ComplainFormDTO.java
+++ src/main/java/com/youprice/onion/dto/board/ComplainFormDTO.java
@@ -10,7 +10,8 @@ public class ComplainFormDTO {
 
     private Long memberId; // 회원번호 FK
     private Long productId; // 상품번호 FK
-    private Long chatroomId; // 채팅방번호 FK
+    @NotEmpty(message = "유형을 선택해주세요.")
     private String complainType; // 신고유형
+    @NotEmpty(message = "내용을 작성해주세요.")
     private String complainContent; // 신고내용
 }
